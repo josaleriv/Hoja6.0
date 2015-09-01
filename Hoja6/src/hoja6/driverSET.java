f9467f3dba5515a1e7cb1845462286f1965375c6
joserivera <joserivera@192.168.0.12>
2015-08-31 22:03:34 -0600

    coso

--- Hoja6/src/hoja6/driverSET.java
+++ Hoja6/src/hoja6/driverSET.java
@@ -10,44 +10,41 @@ public class driverSET {
      */
     public static void main(String[] args) {
         Scanner scan = new Scanner (System.in);
-        SETFactory nfactory = new SETFactory(); // instanciar la Factory
-        AbstractSet desarrolladores = null;
-        String nombre;
-        String grupo;
-        int java;
-        int web;
-        int celulares;
-        int cantjava;
-        int cantweb;
-        int cantcelulares;
-        int opcion;
-        int opcion2;
-        int a=0;
-
-        while (a==0){
-            System.out.println("\nSeleccione una de las siguientes opciones:\n\n1)Tipo de implementacion SET.\n2)Ingresar persona.\n3)Mostrar resultados.\n4)Salir.");
-            System.out.print("\nOpcion: ");
-            opcion = scan.nextInt();
-            if (opcion==1 || opcion==2 || opcion==3){ opcion = opcion; }
-            else if(opcion==4){ System.out.println("\nGracias por utilizar nuestro programa."); a = 1; }
-            else { System.out.println("\nERROR. La opcion ingresada no es valida, recuerde que solo puede ingresar numeros de 1 a 3."); }
-
-            while (opcion==1){ 
-                System.out.println("\nSeleccione una implementacion:\n\n1)HashSet.\n2)TreeSet.\n3)LinkedHashSet.");
-                System.out.print("\nOpcion: ");
-                opcion2 = scan.nextInt();
-                if (opcion2==1 || opcion2==2 || opcion2==3) { opcion=5; desarrolladores=nfactory.implementacion(opcion2); }
-                else { System.out.println("\nERROR. La opcion ingresada no es valida, recuerde que solo puede ingresar numeros de 1 a 3."); }
+        SETFactory factory = new SETFactory(); // instanciar la Factory
+        AbstractSet tiposDesarrolladores = null;
+        String nombre, grupo;
+        int java, web, celulares, cantjava, cantweb, cantcelulares, opcionSET, opcionImplementacion;
+        int x=0;
+        
+        System.out.print("\nBienvenido");
+        while (x==0){
+            System.out.println("\nPor medio del numero, escoja una opcion:\n\n1) Seleccionar tipo de implementacion.\n2) Ingreso de datos.\n3) Desplegar datos.\n4) Salir.");
+            opcionSET = scan.nextInt();
+            if (opcionSET == 1 || opcionSET == 2 || opcionSET == 3){}
+            else if(opcionSET == 4){ 
+                System.out.println("\nSaliendo del programa..."); 
+                x = 1; 
+            }
+            else { 
+                System.out.println("\nERROR. Ingresar una de las opciones."); 
+            }
+            while (opcionSET==1){ 
+                System.out.println("\nPor medio del numero, escoja un tipo de implementacion de SET:\n\n1) HashSet.\n2) TreeSet.\n3) LinkedHashSet.");
+                opcionImplementacion = scan.nextInt();
+                if (opcionImplementacion==1 || opcionImplementacion==2 || opcionImplementacion==3) { 
+                    opcionSET=5; 
+                    tiposDesarrolladores = factory.implementacion(opcionImplementacion); 
+                }
+                else { 
+                    System.out.println("\nERROR. Ingresar el numero de su eleccion."); 
+                }
                 /*
                     desarrollador persona= new desarrollador ("Marlon", 1, 0, 0);
                     desarrolladores.add(persona);
                     Iterator<desarrollador> nombres= desarrolladores.iterator();
                     nombre = nombres.next().getNombre();*/
-                    
-                
-                
             }
-            while (opcion==2){ 
+            while (opcionSET==2){ 
                 System.out.println("\nIngrese el nombre y luego el grupo al que pertenece. Puede ingresar varios grupos, pero estos se deben ingresar en orden y separadas por coma(,)."); 
                 System.out.println("\nNombre: Marlon\nGrupo: 2,3"); 
                 System.out.print("\nNombre:"); 
@@ -57,19 +54,19 @@ public class driverSET {
                 //se pone dos veces la instruccion para que se vacie el buffer 
                 grupo = scan.nextLine();
                 grupo = scan.nextLine();
-                if (grupo.equals("1")){ java=1; web=0; celulares=0;  opcion = 5; SETDesarrollador persona = new SETDesarrollador(nombre, java, web, celulares); desarrolladores.add(persona); }
-                else if (grupo.equals("2")){ java=0; web=1; celulares=0; opcion = 5; SETDesarrollador persona = new SETDesarrollador(nombre, java, web, celulares); desarrolladores.add(persona); }
-                else if (grupo.equals("3")){ java=0; web=0; celulares=1;  opcion = 5; SETDesarrollador persona = new SETDesarrollador(nombre, java, web, celulares); desarrolladores.add(persona); }
-                else if (grupo.equals("1,2")){ java=1; web=1; celulares=0; opcion = 5; SETDesarrollador persona = new SETDesarrollador(nombre, java, web, celulares); desarrolladores.add(persona); }
-                else if (grupo.equals("1,3")){ java=1; web=0; celulares=1; opcion = 5; SETDesarrollador persona = new SETDesarrollador(nombre, java, web, celulares); desarrolladores.add(persona); }
-                else if (grupo.equals("2,3")){ java=0; web=1; celulares=1; opcion = 5; SETDesarrollador persona = new SETDesarrollador(nombre, java, web, celulares); desarrolladores.add(persona); }
+                if (grupo.equals("1")){ java=1; web=0; celulares=0;  opcionSET = 5; SETDesarrollador persona = new SETDesarrollador(nombre, java, web, celulares); tiposDesarrolladores.add(persona); }
+                else if (grupo.equals("2")){ java=0; web=1; celulares=0; opcionSET = 5; SETDesarrollador persona = new SETDesarrollador(nombre, java, web, celulares); tiposDesarrolladores.add(persona); }
+                else if (grupo.equals("3")){ java=0; web=0; celulares=1;  opcionSET = 5; SETDesarrollador persona = new SETDesarrollador(nombre, java, web, celulares); tiposDesarrolladores.add(persona); }
+                else if (grupo.equals("1,2")){ java=1; web=1; celulares=0; opcionSET = 5; SETDesarrollador persona = new SETDesarrollador(nombre, java, web, celulares); tiposDesarrolladores.add(persona); }
+                else if (grupo.equals("1,3")){ java=1; web=0; celulares=1; opcionSET = 5; SETDesarrollador persona = new SETDesarrollador(nombre, java, web, celulares); tiposDesarrolladores.add(persona); }
+                else if (grupo.equals("2,3")){ java=0; web=1; celulares=1; opcionSET = 5; SETDesarrollador persona = new SETDesarrollador(nombre, java, web, celulares); tiposDesarrolladores.add(persona); }
                 else { System.out.println("La forma ingresada de los grupos no es correcta, por favor intente de nuevo. Recuerde que debe ingresar en orden los grupos y separados por coma(,)."); }
             }
-            while (opcion==3){
-                Iterator<SETDesarrollador> nombres= desarrolladores.iterator();
-                Iterator<SETDesarrollador> javas= desarrolladores.iterator();
-                Iterator<SETDesarrollador> webs= desarrolladores.iterator();
-                Iterator<SETDesarrollador> celular= desarrolladores.iterator();
+            while (opcionSET==3){
+                Iterator<SETDesarrollador> nombres= tiposDesarrolladores.iterator();
+                Iterator<SETDesarrollador> javas= tiposDesarrolladores.iterator();
+                Iterator<SETDesarrollador> webs= tiposDesarrolladores.iterator();
+                Iterator<SETDesarrollador> celular= tiposDesarrolladores.iterator();
                 cantjava=0;
                 cantweb=0;
                 cantcelulares=0;
